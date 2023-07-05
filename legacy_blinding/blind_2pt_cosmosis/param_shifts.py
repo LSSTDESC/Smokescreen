bc5168ba7bd84672c06dbe2c828d58141c7cf5cb
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2023-07-05 10:58:02 +0200

    removed the importfrom option

--- legacy_blinding/blind_2pt_cosmosis/param_shifts.py
+++ legacy_blinding/blind_2pt_cosmosis/param_shifts.py
@@ -5,7 +5,7 @@ import importlib
 DEFAULT_PARAM_RANGE = {'cosmological_parameters--sigma8_input':(0.834-3*.04,0.834+3*0.04),\
                        'cosmological_parameters--w':(-1.5,-.5)}
 
-def draw_param_shift(seedstring='blinded', ranges=None, importfrom=None):
+def draw_param_shift(seedstring='blinded', ranges=None):
     """
     Given a string seed, pseudo-randomly draws shift in parameter space.
 
@@ -16,17 +16,6 @@ def draw_param_shift(seedstring='blinded', ranges=None, importfrom=None):
       - ranges is a dictionary with parameter names as keys, the values are
         tuples set up as (min,max). The parameter will be drawn from a flat
         distribution between min and max.
-
-    importfrom:
-    If you'd like to draw from a different distribution, create another .py
-    file with an function in it called draw_paramshift, making sure it return
-    a dictuionary with the same format as below:
-      - If you pass a string with that file's name as an argument importfrom,
-        that distribution will be used instead of this one.
-      - The returned dictionary will be expected to have key 'SHIFTS' associated
-        with a bool to determine how the values in that dictionary are used.
-           pdict['SHIFTS'] = False --> p_shift = pdict['paramname']
-           pdict['SHIFTS'] = True  --> p_shift = p_ref + pdict['paramname']
 
     Make sure any parameters that are shifted here have names matching
     how cosmosis uses them, so that the code in the for loop starting with
@@ -40,22 +29,14 @@ def draw_param_shift(seedstring='blinded', ranges=None, importfrom=None):
     if ranges is None:
         ranges = DEFAULT_PARAM_RANGE
 
-    if importfrom is None:
-        # sorting makes sure it's always the same order
-        params2shift = sorted(ranges.keys())
-        Nparam = len(params2shift)
-        # arrays between 0 and 1
-        shiftfrac = np.random.rand(Nparam)
-        mins, maxs = zip(*[ranges[k] for k in params2shift])
-        dparams = np.array(mins) + (np.array(maxs) - np.array(mins)) * shiftfrac
-        pdict = {param: value for param, value in zip(params2shift, dparams)}
-        pdict['SHIFTS'] = False
-    else:
-        paramdraw = importlib.import_module(importfrom)
-        pdict = paramdraw.draw_paramshift()
+    # sorting makes sure it's always the same order
+    params2shift = sorted(ranges.keys())
+    Nparam = len(params2shift)
+    # arrays between 0 and 1
+    shiftfrac = np.random.rand(Nparam)
+    mins, maxs = zip(*[ranges[k] for k in params2shift])
+    dparams = np.array(mins) + (np.array(maxs) - np.array(mins)) * shiftfrac
+    pdict = {param: value for param, value in zip(params2shift, dparams)}
+    pdict['SHIFTS'] = False
 
-        if 'SHIFTS' not in pdict:
-            raise ValueError("Can't find key 'SHIFTS' in pdict." +
-                             "Set to True if the dict contains Delta params," +
-                             " False if it contains param values.")
     return pdict
--- legacy_blinding/blind_2pt_cosmosis/tests/test_param_shifts.py
+++ legacy_blinding/blind_2pt_cosmosis/tests/test_param_shifts.py
@@ -35,9 +35,5 @@ def test_parameter_shifts_with_default_range():
             assert value >= DEFAULT_PARAM_RANGE[param][0]
             assert value <= DEFAULT_PARAM_RANGE[param][1]
 
-# def test_imported_parameter_shifts():
-#     importfrom = 'other_module'
-#     pdict = draw_param_shift(importfrom=importfrom)
-#     assert isinstance(pdict, dict)
-#     assert 'SHIFTS' in pdict
+
 
