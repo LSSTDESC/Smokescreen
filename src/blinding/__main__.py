fab20e92aeaca0b4c400d4fd99c6408533047b65
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2024-02-29 18:15:12 +0100

    jsonargparse drafts

---
+++ src/blinding/__main__.py
@@ -0,0 +1,16 @@
+from jsonargparse import CLI
+import numpy as np
+from blinding.smokescreen import Smokescreen
+
+def main(name: str, seed: str, output: str):
+    """Main function to run the blinding process.
+
+    Args:
+        name: Name of the module to blind.
+        seed: Seed for the random number generator.
+        output: Path to save the blinded module.
+    """
+    pass
+
+if __name__ == "__main__":
+    CLI()
