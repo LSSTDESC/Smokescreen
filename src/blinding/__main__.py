ddaa6c1bd0656230139ef369a224d482fc83a6f6
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2024-03-01 17:27:52 +0100

    skelleton for CLI is in place

--- src/blinding/__main__.py
+++ src/blinding/__main__.py
@@ -1,16 +1,25 @@
-from jsonargparse import CLI
 import numpy as np
+from typing import Union
+from jsonargparse import CLI
+from jsonargparse.typing import Path_drw, Path_fr
+from pyccl import Cosmology as CosmologyType
+import pyccl as ccl
+
 from blinding.smokescreen import Smokescreen
 
-def main(name: str, seed: str, output: str):
+def main(path_to_sacc: Path_drw, likelihood_path: Path_fr, systematics: dict,
+         shifts_dict: dict, seed: Union[int, str],
+         cosmo: Union[CosmologyType, dict] = ccl.CosmologyVanillaLCDM()):
     """Main function to run the blinding process.
 
     Args:
-        name: Name of the module to blind.
-        seed: Seed for the random number generator.
-        output: Path to save the blinded module.
+        cosmo (dict): Dictionary with the reference cosmological parameters.
+        systematics (dict): Dictionary with the reference firecrown likelihood systematic parameters.
+        likelihood_path (str): Path to the firecrown likelihood module file.
     """
-    pass
+    print("inside main")
+    print(path_to_sacc)
+    print(cosmo._params_init_kwargs)
 
 if __name__ == "__main__":
-    CLI()
+    CLI(main, as_positional=False)
