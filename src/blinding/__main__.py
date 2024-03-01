bc3be1433fdcd92f0be8d92e4dd7c20f065e2a10
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2024-03-01 17:32:02 +0100

    added more docstring

--- src/blinding/__main__.py
+++ src/blinding/__main__.py
@@ -8,14 +8,21 @@ import pyccl as ccl
 from blinding.smokescreen import Smokescreen
 
 def main(path_to_sacc: Path_drw, likelihood_path: Path_fr, systematics: dict,
-         shifts_dict: dict, seed: Union[int, str],
+         shifts_dict: dict, seed: Union[int, str] = 2112,
          cosmo: Union[CosmologyType, dict] = ccl.CosmologyVanillaLCDM()):
-    """Main function to run the blinding process.
+    """Main function to conceal a sacc file using a firecrown likelihood.
+
+    FIXME: Add shift type!
 
     Args:
-        cosmo (dict): Dictionary with the reference cosmological parameters.
-        systematics (dict): Dictionary with the reference firecrown likelihood systematic parameters.
+        path_to_sacc (str): Path to the sacc file to blind.
         likelihood_path (str): Path to the firecrown likelihood module file.
+        systematics (dict): Dictionary with fixed values for the firecrown systematics parameters.
+        shifts_dict (dict): Dictionary with fixed values for the firecrown shifts parameters. 
+            Example: {"Omega_c": (0.20, 0.39), "sigma8": (0.70, 0.90)}
+        seed (int, str): Seed for the blinding process. Defaults to 2112.
+        cosmo (Union[CosmologyType, dict]): Cosmology object or dictionary with cosmological parameters.
+            Defaults to ccl.CosmologyVanillaLCDM().
     """
     print("inside main")
     print(path_to_sacc)
