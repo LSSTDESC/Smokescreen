058a8c5f14082951a08d579bf3e18cf8d40c7134
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2024-06-03 16:09:46 +0200

    added function to construct a cosmology object from a partial dictionary of cosmo params

--- src/blinding/utils.py
+++ src/blinding/utils.py
@@ -1,6 +1,31 @@
 import hashlib
 import numpy as np
 import importlib.util
+import pyccl as ccl
+
+def load_cosmology_from_partial_dict(cosmo_dict):
+    """
+    Given a partial dictionary with cosmological parameters, return a Cosmology 
+    object setting the unspecified parameters to their default values.
+    """
+    # sets the default values for the cosmological parameters
+    cosmo_dict_default = ccl.CosmologyVanillaLCDM()._params_init_kwargs
+    # test that keys in cosmo_dict are in cosmo_dict_default
+    for key in cosmo_dict:
+        if key not in cosmo_dict_default.keys():
+            raise ValueError(f"Invalid cosmology parameter: {key}")
+    for key in cosmo_dict_default:
+        if key in cosmo_dict:
+            # we cannot pass both A_s and sigma8!
+            if key == 'A_s':
+                cosmo_dict_default['sigma8'] = None
+            elif key == 'sigma8':
+                cosmo_dict_default['A_s'] = None
+            cosmo_dict_default[key] = cosmo_dict[key]
+    try:
+        return ccl.Cosmology(**cosmo_dict_default)
+    except ValueError:
+        raise ValueError("Invalid cosmology parameters.")
 
 def load_module_from_path(path):
     """
