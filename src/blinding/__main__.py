6544e64fb8ecc6283097758ef006ceffe4dc4867
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2024-06-03 17:43:38 +0200

    CLI seems to be working, still fixing some final steps

--- src/blinding/__main__.py
+++ src/blinding/__main__.py
@@ -1,16 +1,24 @@
+import os
 import numpy as np
-from typing import Union
+from typing import Union, Dict, Tuple
 from jsonargparse import CLI
 from jsonargparse.typing import Path_drw, Path_fr
 from pyccl import Cosmology as CosmologyType
 import pyccl as ccl
+import sacc
+
 
 from blinding.smokescreen import Smokescreen
+from blinding.utils import load_cosmology_from_partial_dict
 
-def main(path_to_sacc: Path_drw, likelihood_path: Path_fr, systematics: dict,
-         shifts_dict: dict, seed: Union[int, str] = 2112,
+
+def main(path_to_sacc: Path_fr,
+         likelihood_path: str,
+         systematics: dict,
+         shifts_dict: Dict[str, Tuple[float, float]],
+         seed: Union[int, str] = 2112,
          cosmo: Union[CosmologyType, dict] = ccl.CosmologyVanillaLCDM()):
-    """Main function to conceal a sacc file using a firecrown likelihood TEST.
+    """Main function to conceal a sacc file using a firecrown likelihood.
 
     FIXME: Add shift type!
 
@@ -21,12 +29,23 @@ def main(path_to_sacc: Path_drw, likelihood_path: Path_fr, systematics: dict,
         shifts_dict (dict): Dictionary with fixed values for the firecrown shifts parameters. 
             Example: {"Omega_c": (0.20, 0.39), "sigma8": (0.70, 0.90)}
         seed (int, str): Seed for the blinding process. Defaults to 2112.
-        cosmo (Union[CosmologyType, dict]): Cosmology object or dictionary with cosmological parameters.
+        cosmo (Union[CosmologyType, dict]): Cosmology object or dictionary with cosmological parameters you want different than the
+        VanillaLCDM as reference cosmology.
             Defaults to ccl.CosmologyVanillaLCDM().
     """
-    print("inside main")
-    print(path_to_sacc)
-    print(cosmo._params_init_kwargs)
+    # tests if the sacc file exists
+    assert os.path.exists(path_to_sacc), f"File {path_to_sacc} does not exist."
+    assert os.path.exists(likelihood_path), f"File {likelihood_path} does not exist."
+    # reads the sacc file
+    sacc_data = sacc.Sacc.load_fits(path_to_sacc)
+    print(type(likelihood_path))
+    # creates the smokescreen object
+    smoke = Smokescreen(cosmo, systematics, likelihood_path, shifts_dict, sacc_data, seed)
+    # blinds the sacc file
+    smoke.calculate_blinding_factor(factor_type='add')
+    # applies the blinding factor to the sacc file
+    smoke.apply_blinding_to_likelihood_datavec()
+    # saves the blinded sacc file
 
 if __name__ == "__main__":
     CLI(main, as_positional=False)
